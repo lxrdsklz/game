674a4c79e2787feb41a5ef044f99ed8db9c0eafb
lxrdsklz <Git#dsk321#kokiniger>
2021-10-15 23:48:37 +0300

    v0.1.1 fixed

--- source/game_start.py
+++ source/game_start.py
@@ -3,11 +3,18 @@ from source import engine
 
 
 def start():
-    low_edge = int(input('Нижняя граница: '))
-    high_edge = int(input('Верхняя граница: '))
+    ok = True
+    while ok:
+        try:
+            low_edge = int(input('Нижняя граница: '))
+            high_edge = int(input('Верхняя граница: '))
 
-    hidden_number = random.randint(low_edge, high_edge)
+            hidden_number = random.randint(low_edge, high_edge)
 
-    print('Угадайте число от %s до %s' % (low_edge, high_edge))
+            print('Угадайте число от %s до %s' % (low_edge, high_edge))
 
-    engine.game(low_edge, high_edge, hidden_number)
+            engine.game(low_edge, high_edge, hidden_number)
+            ok = False
+        except ValueError as e:
+            print('Укажите число!')
+            ok = True
